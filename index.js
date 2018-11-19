445de260220222ba9b8849ea2a08cc0680fe76d4
congo-quinine <congo.quinine@gmail.com>
2018-11-19 11:02:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = "tracy";
+
+const upperCaseCustomerName = () => {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+const setBestCustomer = () => {
+  bestCustomer = "not bob";
+}
+
+const overwriteBestCustomer = () => {
+  bestCustomer = 'maybe bob'
+}
+
+const changeLeastFavoriteCustomer = () => {
+
+leastFavoriteCustomer = ""
+}
